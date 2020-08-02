f8008dce0a63816b76355df29df2521aa6765a2a
Ryan Skog <ryan.skog@outlook.com>
2020-08-02 12:38:38 -0700

    Creating new workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -21,4 +21,26 @@ module.exports = function(app) {
         res.json(err);
       });
   });
+
+  // Creates new workout with today's date only
+  app.post("/api/workouts", (req, res) => {
+    db.create({})
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    const id = mongojs.ObjectId(req.params.id);
+    db.update({ _id: id }, { $push: { body } })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
